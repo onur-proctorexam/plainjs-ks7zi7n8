21a127d78eb5b26720b338b0a423fa362fa9a93b
onur-proctorexam <onur@proctorexam.com>
2021-08-11 21:15:32 +0000

    First commit

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+// BDD-Style Testing (powered by https://mochajs.org/)
+//
+// Use any assert library
+import chai from "chai/chai.js";
+const expect = chai.expect;
+
+describe("Dummy test", function() {
+  it("should be always true", function() {
+    expect(true).to.be.true;
+  });
+});
